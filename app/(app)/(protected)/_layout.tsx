4ca9af922f7f21e33c1e9ff26aecc98ff3151854
Pol Alcoverro <pol.alcoverro@estudiantat.upc.edu>
2025-04-02 13:28:33 +0200

    Update ProtectedLayout to hide tab bar and add expo-dev-client dependency

--- app/(app)/(protected)/_layout.tsx
+++ app/(app)/(protected)/_layout.tsx
@@ -1,31 +1,22 @@
 import { Tabs } from "expo-router";
 import React from "react";
-
 import { colors } from "@/constants/colors";
 import { useColorScheme } from "@/lib/useColorScheme";
 
 export default function ProtectedLayout() {
-	const { colorScheme } = useColorScheme();
+  const { colorScheme } = useColorScheme();
 
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarStyle: {
-					backgroundColor:
-						colorScheme === "dark"
-							? colors.dark.background
-							: colors.light.background,
-				},
-				tabBarActiveTintColor:
-					colorScheme === "dark"
-						? colors.dark.foreground
-						: colors.light.foreground,
-				tabBarShowLabel: false,
-			}}
-		>
-			<Tabs.Screen name="index" options={{ title: "Home" }} />
-			<Tabs.Screen name="settings" options={{ title: "Settings" }} />
-		</Tabs>
-	);
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          display: "none",
+        },
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    </Tabs>
+  );
 }
